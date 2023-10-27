9717e1c111da56723d8b23e9c0cdbbb3b05fa034
fry <fry@scalepoint.com>
2023-10-27 13:51:38 +0200

    Revert "java: cannot access com.codeborne.selenide.WebDriverRunner"
    This reverts commit f7a5d6f2c3468916fa5d5cfdefb68d930285a7a2.

--- src/main/java/org/example/testdriver/LocalDriverTest.java
+++ src/main/java/org/example/testdriver/LocalDriverTest.java
@@ -1,16 +1,11 @@
 package org.example.testdriver;
 
-import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.webdriver;
 
 public class LocalDriverTest {
     ChromeDriver chromeDriver;
@@ -21,15 +16,14 @@ public class LocalDriverTest {
     public void chromeSetUp() {
 
         chromeDriver = new ChromeDriver(new ChromeOptions());
-        WebDriverRunner.setWebDriver(chromeDriver);
     }
 
     @Test
     public void test() throws InterruptedException {
-        WebDriver driver = webdriver().object();
-        webdriver().object().get("https://www.selenium.dev/documentation/");
+
+        chromeDriver.get("https://www.selenium.dev/documentation/");
         Thread.sleep(3000);
-        String text = $(element).getText();
+        String text = chromeDriver.findElement(element).getText();
         Assert.assertEquals(text, expectedText);
         chromeDriver.quit();
     }
